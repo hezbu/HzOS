a3834c5a63f8b2dc1548eecb27f96f5ad336b244
Hernan Zbucki <hernan.zbucki@gmail.com>
2018-05-08 21:47:32 -0300

    Agrego funcion de copia y llamo a la copia para pasar lo leido en ROM a RAM

--- src/init16.asm
+++ src/init16.asm
@@ -5,8 +5,14 @@ GLOBAL Init16
 ;Etiquetas Externas
 EXTERN Enable_GateA20
 
+%define ROM_SIZE		      (64*1024)
+%define ROM_START_HI_DWORD	0x000F
+%define ROM_START_LO_DWORD	0x0000
+%define RESET_VECTOR	      0xfff0
 
 SECTION .Init16
 Init16:
+      
+      call 
       call Enable_GateA20 		;Se llama a Enable_GateA20 para poder acceder a direccione spor arriba del primer mega
       hlt
---
+++ src/utils16.asm
@@ -0,0 +1,67 @@
+BITS 16
+EXTERN Enable_GateA20
+
+SECTION .Utils16
+Enable_GateA20:
+        push ebp
+        mov ebp, esp
+
+        ;FIXME: Hacer llamada a GateA20
+
+        pop ebp
+        retf
+
+
+
+
+
+; -------------------------------------------------------------------------------
+; memcopy
+; -------------------------------------------------------------------------------
+;
+;void * memcopy(void *destino, const void *origen, unsigned int num_bytes);
+
+memcpy:
+	
+	push ebp        ; Genero stack frame para recuperar los parametros
+	mov ebp, esp    ; Resguardo el ESP
+
+	push edi        ; Salvaguardo los registros que vamos a utilizar
+	push esi        ; Salvaguardo los registros que vamos a utilizar
+	push ecx        ; Salvaguardo los registros que vamos a utilizar
+
+	; La pila en este momento esta conformada
+	;	ebp_h		<---	ebp
+	;	ebp_l
+	;	ip
+	;	arg1_h		<---	ebp+6
+	;	arg1_l
+	;	arg2_h		<---	ebp+10
+	;	arg2_l
+	;	arg3_h		<---	ebp+14
+	;	arg3_l
+
+	mov edi, [ebp+6]        ;  obtengo el destino
+	mov esi, [ebp+10]	;  obtengo el origen
+	mov ecx, [ebp+14]	;  obtengo la cantidad de bytes a copiar
+
+	
+	memcpy_loop:
+		mov ax,[cs:si]
+		mov [es:di],ax
+		add di,2
+		add si,2
+	loop memcpy_loop
+
+
+	pop dword ecx           ;Recupero los valores de la funcion caller
+	pop dword esi           ;Recupero los valores de la funcion caller
+	pop dword edi           ;Recupero los valores de la funcion caller        
+	pop dword ebp           ;Recupero los valores de la funcion caller        
+
+	mov eax, [esp+2]        
+
+	ret                     ;  Popeo IP
+
+
+
